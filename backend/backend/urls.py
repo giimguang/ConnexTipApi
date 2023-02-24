b3ee5981ae91945935efff327a20c659592cc54c
giimguang <giimguangmao@gmail.com>
2023-02-24 19:17:16 +0700

    Collect static

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
