bfe890f1808dd737d106005e20de9660b26bdf72
giimguang <giimguangmao@gmail.com>
2023-02-26 20:45:09 +0700

    Change cors policy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,7 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS
 # CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
+CORS_ORIGIN_WHITELIST = [
     'https://spontaneous-tanuki-7902ac.netlify.app/',
     'http://localhost:3000',
 ]
