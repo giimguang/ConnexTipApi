3f234f2b4c84b390f0cfdf1643236f36985e5da7
giimguang <giimguangmao@gmail.com>
2023-03-10 21:32:01 +0700

    Bug fix

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -11,7 +11,7 @@ class Article(models.Model):
     slug = models.CharField(max_length=20, unique=True, null=True)
     viewer = models.IntegerField(default=0)
     tags = models.ManyToManyField(Tag, related_name="tagged")
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='category_articles', null=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='category_articles', blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     
@@ -34,3 +34,6 @@ class Category(models.Model):
     title = models.CharField(max_length=255, unique=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
