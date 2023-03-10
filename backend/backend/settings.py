7bd0c7e94ce61b787e28893b1a5c2c925326b005
giimguang <giimguangmao@gmail.com>
2023-03-10 11:40:56 +0700

    Update allow_host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,10 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == "True"
 
-ALLOWED_HOSTS = [
-    '127.0.0.1', # For development
-    'https://connextipapi.onrender.com/'
-]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
